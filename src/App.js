dd324ecef5f99e42b586f4c2dedac201de403b09
Dhivya Pradap <pradapdp7@gmail.com>
2024-02-22 17:32:41 +0530

    2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'; 
 import Photos from './Components/Photos';
 import Favourites from './Components/Favourites';
-import { BrowserRouter,Routes,Route,Link } from 'react-router-dom';
+import { FaSearch } from 'react-icons/fa';
 
 function App() {
   return (
@@ -12,18 +13,24 @@ function App() {
           <div className='Navbar_logo'>
             Fotoflix
           </div>
+          <form action="" className="Navbar_search">
+            <input type="text" className="form-input" placeholder='search' />
+            <button type="submit" className="submit-btn">
+              <FaSearch/>
+            </button>
+          </form>
           <div className='Navbar_links'>
             <Link to="/">Home</Link>
-            <Link to="/favourites">favourites</Link>
+            <Link to="/Favourites">Favourites</Link>
           </div>
         </nav>
         <Routes>
-          <Route path="/" element={<Photos />}/>
-          <Route path="/favourites" element={<Favourites />}/>
+          <Route path="/" element={<Photos />} />
+          <Route path="/Favourites" element={<Favourites />} />
         </Routes>
       </div>
     </BrowserRouter>
   );
 }
- 
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom'; // Change import statement here
 import './index.css';
 import App from './App';
 
@@ -9,5 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-
