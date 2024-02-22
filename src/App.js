2ed112513eac333c6e0b196e61dd06330a1d076e
Dhivya Pradap <pradapdp7@gmail.com>
2024-02-22 10:15:57 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import React from 'react';
+import Photos from './Components/Photos';
+import Favourites from './Components/Favourites';
+import { BrowserRouter,Routes,Route,Link } from 'react-router-dom';
 
 function App() {
   return (
-   <h1>welcome</h1>
+    <BrowserRouter>
+      <div>
+        <nav className='Navbar'>
+          <div className='Navbar_logo'>
+            Fotoflix
+          </div>
+          <div className='Navbar_links'>
+            <Link to="/">Home</Link>
+            <Link to="/favourites">favourites</Link>
+          </div>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Photos />}/>
+          <Route path="/favourites" element={<Favourites />}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
-
+ 
 export default App;
